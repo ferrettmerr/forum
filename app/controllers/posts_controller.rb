f6339b52c91b4e198f1eb4e4449c17edd9eb6a24
Larry Brewer <ferrettmerr@gmail.com>
2013-04-20 18:12:11 -0400

    Initial finish of project, pending professors approval

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,24 @@ class PostsController < ApplicationController
 		@thread = Post.find_by_id(params[:id])
 		impressionist(@thread) 
 	end
-	def create
 
+	def new
+		@post = Post.new
+		if params.has_key? :post_id
+			@thread_id = params[:post_id]
+		end
+	end
+
+	def create
+		@post = Post.new(params[:post])
+		@post.user = current_user
+      if @post.save
+      	if @post.thread.nil?
+					redirect_to :action => 'show', id: @post.id
+      	else
+					redirect_to :action => 'show', id: @post.thread.id        
+	      end
+      end
 	end
 
 	def delete
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   has_many :posts, :class_name => "Post", :foreign_key => :thread_id
   belongs_to :thread, :class_name => "Post", :counter_cache => true
   belongs_to :user
-  attr_accessible :message, :title, :posts, :thread, :user
+  attr_accessible :message, :title, :posts, :thread, :thread_id, :user
   is_impressionable
 
   def self.threads
